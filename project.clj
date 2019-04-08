8a41e05fa64eb7e6308a8ce03091bdb664d623c1
Igor Bondarenko <jetmind2@gmail.com>
2019-04-08 13:54:09 +0300

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject fe-challenge "1.0"
+  :description "Kasta FrontEnd challenge"
+  :url "https://github.com/kasta-ua/fe-challenge"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :main kasta.core
+
+  :dependencies [[org.clojure/clojure "1.10.0"]])
---
+++ src/kasta/core.clj
@@ -0,0 +1,4 @@
+(ns kasta.core)
+
+(defn -main [& args]
+  (println "TODO"))
